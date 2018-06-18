15f809c9ac8485fe4f1c10b7386b2e670e9e8c5a
chuckolate <chukho.wu@gmail.com>
2018-06-18 14:20:22 -0400

    add AuthorSchema for author model

---
+++ models/author.js
@@ -0,0 +1,8 @@
+var AuthorSchema = new Schema(
+  {
+    first_name: {type: String, required: true, max: 100},
+    family_name: {type: String, required: true, max: 100},
+    date_of_birth: {type: Date},
+    date_of_death: {type: Date},
+  }
+);
